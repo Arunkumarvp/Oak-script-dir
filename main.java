a2290777791bdcfa59bdce1ce695b6e398523451
Arunkumarvp <arunkumarvp2001@gmail.com>
2022-08-28 18:45:04 +0530

    automata fix  set

---
+++ main.java
@@ -0,0 +1,20 @@
+public class main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5;) {
+            System.out.println("OakScript");
+            // break; now the break is comment 
+
+        }
+    }
+
+}
+public class main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5;) {
+            System.out.println("OakScript");
+            break;
+
+        }
+    }
+
+}
---
+++ thirdautomata.java
@@ -0,0 +1,10 @@
+public class thirdautomata {
+    static int result() {
+        private int a = 0;
+        return a++;
+    }
+
+    public static void main(String[] args) {
+        System.out.print(result());
+    }
+}
